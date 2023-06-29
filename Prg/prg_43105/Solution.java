d2c74841a21496b23cd7ca2c3070dce742146e03
Jiyeon Lee <74610908+thegreatjy@users.noreply.github.com>
2023-06-29 14:03:39 +0900

    프로그래머스-정수삼각형 다시 풀기

--- Prg/prg_43105/Solution.java
+++ Prg/prg_43105/Solution.java
@@ -1,39 +1,39 @@
 package prg_43105;
 
+import java.util.*;
+
 public class Solution {
 	public int solution(int[][] triangle) {
-		int height = triangle.length;
-        int[][] dp = new int[height][height];	//dp[r][c] == (r,c)까지의 최대 누적합
-        
-        int r = 0, result = 0;
-        dp[0][0] = triangle[0][0];
-        while(r <= height-2) {
-        	for(int i=0;i<=r;i++) {
-	        	//왼쪽 아래 
-	        	dp[r+1][i] = Math.max(dp[r+1][i], dp[r][i] + triangle[r+1][i]);
-	        	//오른쪽 아래 
-	        	dp[r+1][i+1] = Math.max(dp[r+1][i+1], dp[r][i] + triangle[r+1][i+1]);
-	        	//맨 아랫줄에서 제일 큰 수가 정답 
-	        	if(r == height-2) {
-	        		result = Math.max(result, dp[r+1][i]);
-	        	}
-        	}
-        	//아래층으로 이동 
-        	++r;
-        }
-        
-        result = Math.max(result, dp[height-1][height-1]);
-        return result;
-    }
+		int preMax = 0, result = 0;
+		int r = triangle.length;
+		int[][] max = new int[r][r];
+		max[0][0] = triangle[0][0];
+		
+		for(int i=1;i<r;i++) {
+			for(int j=0;j<=i;j++) {
+				if(j-1<0) {	//왼쪽 위가 없다.
+					preMax = max[i-1][j];
+				}else {
+					//왼쪽 위vs오른쪽 위
+					preMax = Math.max(max[i-1][j-1], max[i-1][j]);
+				}
+				max[i][j] = preMax + triangle[i][j];
+				
+				//마지막층에서는 그 층에서의 최댓값을 구함
+				if(i==r-1) {
+					result = Math.max(max[i][j], result);
+				}
+			}
+		}
+		
+		return result;
+	}
 	
 	public static void main(String[] args) {
-		int[][] triangle = {
-				{7},
-				{3, 8},
-				{8, 1, 0},
-				{2, 7, 4, 4},
-				{4, 5, 2, 6, 5}
-		};
-		System.out.println(new Solution().solution(triangle));
+		// TODO Auto-generated method stub
+		int[][] triangle = {{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}};
+		int result = new Solution().solution(triangle);
+		System.out.println(result);
 	}
+
 }
