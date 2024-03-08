70d1d2fbce628c5d3fb01487351e5634481d865d
Jiyeon Lee <74610908+thegreatjy@users.noreply.github.com>
2024-03-08 23:50:24 +0900

    프로그래머스-3월에 태어난 여성 회원 목록 출력하기

---
+++ Prg/prg_131120/solution.sql
@@ -0,0 +1,6 @@
+SELECT MEMBER_ID, MEMBER_NAME, GENDER, date_format(DATE_OF_BIRTH, '%Y-%m-%d') as DATE_OF_BIRTH
+from MEMBER_PROFILE
+where gender = 'W'
+    and date_format(DATE_OF_BIRTH, '%m') = '03'
+    and TLNO is not null
+order by MEMBER_ID asc;
