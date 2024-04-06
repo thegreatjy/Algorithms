f4542b0a16f02009596623c814d2649491fccc23
Jiyeon Lee <74610908+thegreatjy@users.noreply.github.com>
2024-04-06 22:48:51 +0900

    프로그래머스-귤 고르기

---
+++ Prg/prg_138476/Solution.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        // Map<귤 크기, 개수>
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i< tangerine.length; i++){
+            map.put(tangerine[i], map.getOrDefault(tangerine[i], 0) + 1);
+        }
+
+        // 귤 크기 리스트
+        List<Integer> keyList = new ArrayList<>(map.keySet());
+        // 귤 개수 내림차순으로 정렬
+        keyList.sort((o1, o2) -> map.get(o2) - map.get(o1));
+
+        // 뒤에서부터 k개 고르기
+        int sum = 0, result = 0;
+        for(Integer key: keyList){
+            if(sum >= k){
+                break;
+            }
+            sum += map.get(key);
+            ++result;
+        }
+
+        return result;
+    }
+}
