9f89f47fc9cbbc059e0808268f8714117f69cc9e
thegreatjy <thegreatjy@gmail.com>
2024-09-24 21:34:47 +0900

    프로그래머스-조건에 맞는 사용자와 총 거래금액 조회하기

---
+++ Prg/prg_164668/solution.sql
@@ -0,0 +1,11 @@
+-- 완료된 중고 거래의 총금액이 70만 원 이상인 사람
+-- 정답
+SELECT u.USER_ID, u.NICKNAME, sum(b.price) AS TOTAL_SALES
+FROM USED_GOODS_BOARD b
+JOIN USED_GOODS_USER u
+ON b.WRITER_ID = u.USER_ID
+WHERE status = 'DONE'
+GROUP BY u.USER_ID
+HAVING sum(b.PRICE) >= 700000
+ORDER BY TOTAL_SALES asc
+;
