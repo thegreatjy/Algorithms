4718b4c01f6366a8456703434cfbd8cd42a196e2
thegreatjy <thegreatjy@gmail.com>
2024-10-04 16:32:36 +0900

    프로그래머스-조건별로 분류하여 주문상태 출력하기

---
+++ Prg/prg_131113/solution.sql
@@ -0,0 +1,13 @@
+-- 코드를 입력하세요
+-- 2022년 5월 1일을 기준으로 주문 ID, 제품 ID, 출고일자, 출고여부를 조회
+SELECT ORDER_ID, PRODUCT_ID, DATE_FORMAT(OUT_DATE, '%Y-%m-%d') AS OUT_DATE,
+       (
+           CASE
+               WHEN OUT_DATE <= '2022-05-01' THEN '출고완료'
+               WHEN OUT_DATE IS NULL THEN '출고미정'
+               ELSE '출고대기'
+               END
+           ) AS '출고여부'
+FROM FOOD_ORDER
+ORDER BY ORDER_ID asc
+;
